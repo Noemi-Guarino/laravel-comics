7bb0d8884c793a39de8d2866b838af93bb9aff8e
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-02-24 01:09:01 +0100

    edit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<!-- @php
+ @php
     $links = [
         [
             'url' => '/',
@@ -6,39 +6,86 @@
             'active' => true,
         ],
         [
-            'url' => '/chi-siamo',
-            'label' => 'Chi siamo',
+            'url' => '/charachters',
+            'label' => 'charachters',
             'active' => true,
         ],
         [
-            'url' => '/contatti',
-            'label' => 'Contatti',
+            'url' => '/comics',
+            'label' => 'comics',
             'active' => false,
         ],
+        [
+            'url' => '/movies',
+            'label' => 'movies',
+            'active' => false,
+        ],
+        [
+            'url' => '/tv',
+            'label' => 'tv',
+            'active' => false,
+        ],
+        [
+            'url' => '/games',
+            'label' => 'games',
+            'active' => false,
+        ],
+        [
+            'url' => '/collectibles',
+            'label' => 'collectibles',
+            'active' => false,
+        ],
+        [
+            'url' => '/videos',
+            'label' => 'videos',
+            'active' => false,
+        ],
+        [
+            'url' => '/fans',
+            'label' => 'fans',
+            'active' => false,
+        ],
+        [
+            'url' => '/news',
+            'label' => 'news',
+            'active' => false,
+        ],
+        [
+            'url' => '/shop',
+            'label' => 'shop',
+            'active' => false,
+        ],
+
+
     ];
 @endphp
 
-<header>
-    <nav>
-        questo è il mio header
-        <ul>
-            @foreach ($links as $link)
-                <li>
-                    @if ($link['active'])
-                        <a href="{{ $link['url'] }}">
-                            {{ $link['label'] }}
-                        </a>
-                    @else
-                        <del>
-                            {{ $link['label'] }}
-                        </del>
-                    @endif
-                </li>
-            @endforeach
-        </ul>
-    </nav>
-</header> -->
 
 <header>
-    QUESTO è IL MIO HEADER
+    <div class="container d-flex align-items-center justify-content-between p-4">
+        <div class="img_logo">
+            <img class="gatto-img" src="{{ Vite::asset('public/images/dc-logo.png') }}" alt="Gatto"> 
+        </div>
+        <div class=>
+            <nav>
+                <ul class="d-flex align-items-center text-decoration-none ">
+                    @foreach ($links as $link)
+                    <li class="p-2">
+                        <a class="text-decoration-none" href="{{ $link['url'] }}">
+                            {{ $link['label'] }}
+                        </a>
+                    </li>
+                    @endforeach
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="inputsection">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" id="search" name="search">
+        </div>
+    </div>
+    <div>
+        <img class="gatto-img" src="{{ Vite::asset('public/images/dc-logo.png') }}" alt="Gatto"> 
+    </div>
 </header>
